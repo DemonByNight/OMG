67cdd2a3cd28ffe69cd0bb51d32ef26e8b764dbd
Pavel Kulich <shlakva@mail.ru>
2024-08-15 10:09:53 +0300

    Prepare block for use

---
+++ OMG/Assets/Scripts/Blocks/UIBlock.cs
@@ -0,0 +1,21 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace OMG
+{
+    public class UIBlock : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer sr;
+        [SerializeField] private UIBlockAnimatorStateHandler animationStateHandler;
+
+        public void SetOrder(int order)
+        {
+            sr.sortingOrder = order;
+        }
+
+        public async UniTask PlayDestroyEffectAsync()
+        {
+            await animationStateHandler.SetDestroyAsync().Task;
+        }
+    }
+}
